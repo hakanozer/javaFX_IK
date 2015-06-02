8342a981a871f6c930e8c379f9a74f4d480efc3d
Recep Çoban <1306100066@ogr.iu.edu.tr>
2015-06-02 16:28:30 +0300

    Pencere adı atandı.
    stage.setTitle("JavaFX IK Projesi");

--- src/javafx_ik/JavaFX_IK.java
+++ src/javafx_ik/JavaFX_IK.java
@@ -22,7 +22,7 @@ public class JavaFX_IK extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
+        stage.setTitle("JavaFX IK Projesi");
         stage.setScene(scene);
         stage.show();
     }
