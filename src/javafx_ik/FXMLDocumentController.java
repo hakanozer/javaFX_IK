548e9f572f1a097be79d09d0bcb6d43f0b6a82e5
Hakan ÖZER <hakanozer02@gmail.com>
2015-06-02 17:07:57 +0300

    Degis
    Degis

--- src/javafx_ik/FXMLDocumentController.java
+++ src/javafx_ik/FXMLDocumentController.java
@@ -7,7 +7,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import static javax.swing.text.html.HTML.Tag.HEAD;
 
 
 public class FXMLDocumentController implements Initializable {
@@ -17,27 +16,14 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        System.out.println("Butona tıklayınız");
-=======
-        System.out.println("Butona tıklayın");
-<<<<<<< HEAD
->>>>>>> origin/master
 
-=======
-<<<<<<< HEAD
->>>>>>> 734022c9acc3e39e243ab461b4ff2bbb4e2c999c
-        label.setText("Merhaba Dünya.");
-=======
-        System.out.println("Bence tıklamayın");
-        label.setText("Hello World!");
->>>>>>> origin/master
-=======
+
+        System.out.println("Butona tıklayın");
+
         
         System.out.println("Butona tıklayın");
 
->>>>>>> 22d823443b3bb08b6786473d5e4251001119a881
+
     }
     
     @Override
