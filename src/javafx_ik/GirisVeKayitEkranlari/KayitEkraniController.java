5bc9b5993100292760bfee57d4c49cd0647a8e9c
Ensar YAZICI <ensar.yaz@gmail.com>
2015-06-04 15:51:49 +0300

    Kayıt Ol ve Giriş Yap Pencereleri

---
+++ src/javafx_ik/GirisVeKayitEkranlari/KayitEkraniController.java
@@ -0,0 +1,71 @@
+package javafx_ik.GirisVeKayitEkranlari;
+
+import java.net.URL;
+import java.security.MessageDigest;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx_ik.Kullanicilar.Kisiler;
+
+public class KayitEkraniController extends Kisiler implements Initializable {
+Metodlar m = new Metodlar();
+    public void choices() {
+
+        seviyeSec.getItems().addAll("Admin", "İK Uzmanı", "Firma", "Personel");
+    }
+
+    @FXML
+    private TextField kadi;
+    @FXML
+    private PasswordField parola;
+    @FXML
+    private Button btnKaydet;
+    @FXML
+    private Label label;
+    @FXML
+    private ChoiceBox seviyeSec;
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println(seviyeSec.getSelectionModel().getSelectedIndex());
+        String pass = parola.getText();
+        String user = kadi.getText();
+        int seviye = 0;
+        if (seviyeSec.getSelectionModel().getSelectedIndex() == 0) {
+            seviye = 0;
+        } else if (seviyeSec.getSelectionModel().getSelectedIndex() == 1) {
+            seviye = 1;
+        } else if (seviyeSec.getSelectionModel().getSelectedIndex() == 2) {
+            seviye = 2;
+        } else if (seviyeSec.getSelectionModel().getSelectedIndex() == 3) {
+            seviye = 3;
+        }
+
+        if (user.equals("") || pass.equals("")) {
+            label.setText("Boş Alan Bırakmayın!");
+        } else {
+
+            if (kayitOl(user, m.md5Olustur(pass), seviye)) {
+              // kayıt başarılı ise
+                // kaydı yapan kişinin kendi sayfası devam edecek
+            }
+
+        }
+
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        choices(); // ChoiceBox dolduruluyor.
+    }
+
+}
--- src/javafx_ik/Kullanicilar/Kisiler.java
+++ src/javafx_ik/Kullanicilar/Kisiler.java
@@ -5,8 +5,9 @@ import javafx_ik.msDB;
 
 public abstract class Kisiler extends msDB {
 
-    public void kayitOl(String kul_adi, String kul_sifre, int seviye) {
+    public boolean kayitOl(String kul_adi, String kul_sifre, int seviye) {
         // kayıt kul_adi ve şifre ile
+        boolean sonuc = false;
         try {
 
             int durum = baglan().executeUpdate("INSERT INTO kisiler values ('" + kul_adi + "', '" + kul_sifre + "', '" + seviye + "', null)");
@@ -17,6 +18,7 @@ public abstract class Kisiler extends msDB {
                         int profil = rs.getInt("id");
                         System.out.println(profil);
                         int durumProfil = baglan().executeUpdate("UPDATE kisiler SET profil_id = '" + profil + "' WHERE id = (SELECT MAX(id) FROM kisiler)");
+                        sonuc = durumProfil > 0 ? true : false;
                     }
                 } else if (seviye == 2) {
                     ResultSet rs = baglan().executeQuery("(SELECT id FROM firmalar WHERE id = (SELECT MAX(id) FROM firmalar))");
@@ -24,6 +26,7 @@ public abstract class Kisiler extends msDB {
                         int profil = rs.getInt("id");
                         System.out.println(profil);
                         int durumProfil = baglan().executeUpdate("UPDATE kisiler SET profil_id = '" + profil + "' WHERE id = (SELECT MAX(id) FROM kisiler)");
+                        sonuc = durumProfil > 0 ? true : false;
                     }
                 } else if (seviye == 3) {
                     ResultSet rs = baglan().executeQuery("(SELECT id FROM personel WHERE id = (SELECT MAX(id) FROM personel))");
@@ -31,26 +34,38 @@ public abstract class Kisiler extends msDB {
                         int profil = rs.getInt("id");
                         System.out.println(profil);
                         int durumProfil = baglan().executeUpdate("UPDATE kisiler SET profil_id = '" + profil + "' WHERE id = (SELECT MAX(id) FROM kisiler)");
+                        sonuc = durumProfil > 0 ? true : false;
                     }
                 } else {
                     System.out.println("ADMIN kayıt!");
                 }
             }
+            System.out.println("Kayıt Başarılı");
+            sonuc = true;
         } catch (Exception e) {
             System.err.println("Kayıt Hatası : " + e);
+            sonuc = false;
         }
+
+        return sonuc;
+
     }
 
-    public void girisYap(String kul_adi, String kul_sifre) {
+    public int[] girisYap(String kul_adi, String kul_sifre) {
         // girişyap kul_adi
+        int sevID[] = new int[2];
         try {
             ResultSet rs = baglan().executeQuery("SELECT *FROM kisiler WHERE kul_adi='" + kul_adi + "' AND kul_sifre ='" + kul_sifre + "'");
             if (rs.next()) {
                 System.out.println("Giriş Başarılı!");
+                sevID[0] = rs.getInt("seviye");
+                sevID[1] = rs.getInt("profil_id");
+
             }
         } catch (Exception e) {
             System.err.println("Giriş Yapılamadı : " + e);
         }
+        return sevID;
     }
 
 }
