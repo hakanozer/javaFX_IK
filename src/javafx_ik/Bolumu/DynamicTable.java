e6fc3e37c563c7350b3135ffcf367fb756aff343
Aykut Yüksek <aykut.yuksek.92@gmail.com>
2015-06-04 18:26:58 +0300

    firma yönetimi
    devam ediliyor...

--- src/javafx_ik/Bolumu/DynamicTable.java
+++ src/javafx_ik/Bolumu/DynamicTable.java
@@ -8,89 +8,69 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableView;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Callback;
 import javafx_ik.msDB;
 
-public class DynamicTable extends Application{
+public class DynamicTable extends Application {
 
-    //TABLE VIEW AND DATA
     private ObservableList<ObservableList> data;
     private TableView tableview;
-    msDB ms=new msDB();
+    msDB ms = new msDB();
 
-    //MAIN EXECUTOR
     public static void main(String[] args) {
         launch(args);
         //recep çoban, echo "coban was here";
     }
 
-    //CONNECTION DATABASE
-    public void buildData(String tablo){
-          Connection c ;
-          data = FXCollections.observableArrayList();
-          try{
+    public void buildData(String tablo) {
+        Connection c;
+        data = FXCollections.observableArrayList();
+        try {
             c = ms.baglan().getConnection();
-                    //SQL FOR SELECTING ALL OF CUSTOMER
-            String SQL = "SELECT * from " + tablo;
-            //ResultSet
+            String SQL = "SELECT * FROM " + tablo;
             ResultSet rs = c.createStatement().executeQuery(SQL);
 
-            /**********************************
-             * TABLE COLUMN ADDED DYNAMICALLY *
-             **********************************/
-            for(int i=0 ; i<rs.getMetaData().getColumnCount(); i++){
-                //We are using non property style for making dynamic table
-                final int j = i;                
-                TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i+1));
-                col.setCellValueFactory(new Callback<CellDataFeatures<ObservableList,String>,ObservableValue<String>>(){                    
-                    public ObservableValue<String> call(CellDataFeatures<ObservableList, String> param) {                                                                                              
-                        return new SimpleStringProperty(param.getValue().get(j).toString());                        
-                    }                    
+            for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
+                final int j = i;
+                TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i + 1));
+                col.setCellValueFactory(new Callback<CellDataFeatures<ObservableList, String>, ObservableValue<String>>() {
+                    public ObservableValue<String> call(CellDataFeatures<ObservableList, String> param) {
+                        return new SimpleStringProperty(param.getValue().get(j).toString());
+                    }
                 });
-               
-                tableview.getColumns().addAll(col); 
-                System.out.println("Column ["+i+"] ");
+
+                tableview.getColumns().addAll(col);
+                System.out.println("Column [" + i + "] ");
             }
 
-            /********************************
-             * Data added to ObservableList *
-             ********************************/
-            while(rs.next()){
-                //Iterate Row
+            while (rs.next()) {
                 ObservableList<String> row = FXCollections.observableArrayList();
-                for(int i=1 ; i<=rs.getMetaData().getColumnCount(); i++){
-                    //Iterate Column
+                for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
                     row.add(rs.getString(i));
                 }
-                System.out.println("Row [1] added "+row );
+                System.out.println("Row [1] added " + row);
                 data.add(row);
-
             }
-
-            //FINALLY ADDED TO TableView
             tableview.setItems(data);
-          }catch(Exception e){
-              e.printStackTrace();
-              System.out.println("Error on Building Data");             
-          }
-      }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error on Building Data");
+        }
+    }
 
-    
-      @Override
-      public void start(Stage stage) throws Exception {
-        //TableView
+    @Override
+    public void start(Stage stage) throws Exception {
         tableview = new TableView();
         buildData("firmalar");
-
-        //Main Scene
-        Scene scene = new Scene(tableview);        
-
+        Scene scene = new Scene(tableview);
         stage.setScene(scene);
         stage.show();
-      }
+    }
 }
