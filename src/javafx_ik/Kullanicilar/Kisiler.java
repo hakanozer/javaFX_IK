698ea5c361069e5afee378a8a391fd1d24b320c3
Ensar YAZICI <ensar.yaz@gmail.com>
2015-06-04 13:57:07 +0300

    Kayıt ve Giriş metodları yazıldı.
    Admin, İK Uzmanı, Firmalar ve Personel sınıfları için kayitOl() ve
    girisYap() metodları yazıldı.

--- src/javafx_ik/Kullanicilar/Kisiler.java
+++ src/javafx_ik/Kullanicilar/Kisiler.java
@@ -1,6 +1,10 @@
 package javafx_ik.Kullanicilar;
 
-public abstract class Kisiler {
+import java.sql.ResultSet;
+import javafx_ik.msDB;
+
+public abstract class Kisiler extends msDB {
+
     private String kul_adi;
     private String kul_sifre;
     private int seviye;
@@ -10,39 +14,64 @@ public abstract class Kisiler {
         return kul_adi;
     }
 
-    public void setKul_adi(String kul_adi) {
-        this.kul_adi = kul_adi;
-    }
-
     public String getKul_sifre() {
         return kul_sifre;
-    }
-
-    public void setKul_sifre(String kul_sifre) {
-        this.kul_sifre = kul_sifre;
     }
 
     public int getSeviye() {
         return seviye;
     }
 
-    public void setSeviye(int seviye) {
-        this.seviye = seviye;
-    }
-
     public int getProfil_id() {
         return profil_id;
     }
 
-    public void setProfil_id(int profil_id) {
-        this.profil_id = profil_id;
+    public void kayitOl(String kul_adi, String kul_sifre, int seviye) {
+        // kayıt kul_adi ve şifre ile
+        try {
+
+            int durum = baglan().executeUpdate("INSERT INTO kisiler values ('" + kul_adi + "', '" + kul_sifre + "', '" + seviye + "', null)");
+            if (durum > 0) {
+                if (seviye == 1) {
+                    ResultSet rs = baglan().executeQuery("(SELECT id FROM ik_uzmani WHERE id = (SELECT MAX(id) FROM ik_uzmani))");
+                    if (rs.next()) {
+                        int profil = rs.getInt("id");
+                        System.out.println(profil);
+                        int durumProfil = baglan().executeUpdate("UPDATE kisiler SET profil_id = '" + profil + "' WHERE id = (SELECT MAX(id) FROM kisiler)");
+                    }
+                } else if (seviye == 2) {
+                    ResultSet rs = baglan().executeQuery("(SELECT id FROM firmalar WHERE id = (SELECT MAX(id) FROM firmalar))");
+                    if (rs.next()) {
+                        int profil = rs.getInt("id");
+                        System.out.println(profil);
+                        int durumProfil = baglan().executeUpdate("UPDATE kisiler SET profil_id = '" + profil + "' WHERE id = (SELECT MAX(id) FROM kisiler)");
+                    }
+                } else if (seviye == 3) {
+                    ResultSet rs = baglan().executeQuery("(SELECT id FROM personel WHERE id = (SELECT MAX(id) FROM personel))");
+                    if (rs.next()) {
+                        int profil = rs.getInt("id");
+                        System.out.println(profil);
+                        int durumProfil = baglan().executeUpdate("UPDATE kisiler SET profil_id = '" + profil + "' WHERE id = (SELECT MAX(id) FROM kisiler)");
+                    }
+                } else {
+                    System.out.println("ADMIN kayıt!");
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Kayıt Hatası : " + e);
+        }
     }
-    
-    
-    
-    public abstract void kayitOl();
-    
-    public abstract void girisYap();
-    
-    
+
+    public void girisYap(String kul_adi, String kul_sifre) {
+        // girişyap kul_adi
+        try {
+            ResultSet rs = baglan().executeQuery("SELECT *FROM kisiler WHERE kul_adi='" + kul_adi + "' AND kul_sifre ='" + kul_sifre + "'");
+            if (rs.next()) {
+                System.out.println("Giriş Başarılı!");
+            }
+        } catch (Exception e) {
+            System.err.println("Giriş Yapılamadı : " + e);
+        }
+    }
+
 }
