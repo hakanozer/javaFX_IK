613b5afddec11eadbae5db14efdd8b2f60a49f8a
SEYİT ALTUNKAYNAK <seyitaltunkaynak@hotmail.com>
2015-06-04 14:42:32 +0300

    ik tasarım
    açıklama yok

---
+++ src/javafx_ik/Bolumu/FirmaYonetimiController.java
@@ -0,0 +1,19 @@
+
+package javafx_ik.Bolumu;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx_ik.msDB;
+
+
+public class FirmaYonetimiController extends msDB implements Initializable {
+
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    
+}
