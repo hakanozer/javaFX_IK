13420269ac87b7753938aa5f67ad96ef5df23fd2
myemir <m.yasar.emir@gmail.com>
2015-06-08 18:13:01 +0300

    admin puan görüntüleme

---
+++ src/javafx_ik/Admin/ÖdemeBelirlemeController.java
@@ -0,0 +1,144 @@
+package javafx_ik.Admin;
+
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx_ik.Kullanicilar.Firmalar;
+import javafx_ik.msDB;
+
+public class ÖdemeBelirlemeController extends msDB implements Initializable {
+
+    ArrayList<String> alist = new ArrayList<>();
+  
+    
+     static ObservableList<Firmalar> firmaListe = FXCollections.observableArrayList();
+
+
+    public ObservableList<Firmalar> firmabilgileriGetir() {
+        try {
+            ResultSet rs = baglan().executeQuery("select * from firmalar");
+
+            while (rs.next()) {
+               // System.out.println("fgdfgdf");
+                firmaListe.add(new Firmalar(rs.getInt("id"), rs.getInt("bolum_id"), rs.getString("unvan"), rs.getString("yetkili_adi"), rs.getString("yetkili_soyadi"), rs.getString("telefon"), rs.getString("gsm"), rs.getString("logo"), rs.getInt("puan"),rs.getString("aciklama"),rs.getString("web_adresi"),rs.getString("mail"),rs.getString("tarih")));
+            }
+            baglan().close();
+
+        } catch (Exception e) {
+            System.err.println("Data Getirme Hatası : " + e);
+        }
+      
+        return firmaListe;
+    }
+
+    @FXML
+    Label lblBedel;   
+    
+    @FXML
+    ComboBox cmbAd;
+
+    @FXML
+    TextField txtFirmaPuan;
+
+    @FXML
+    TextField txtİlanPuan;
+
+    @FXML
+    public void fpuanGetir(ActionEvent evt) {
+        String adi = (String) cmbAd.getSelectionModel().getSelectedItem();
+        
+        try {
+            ResultSet rs = baglan().executeQuery("select puan from firmalar where unvan= '" + adi + "' ");
+            while (rs.next()) {
+                int comboID = cmbAd.getSelectionModel().getSelectedIndex();
+
+                int i = 0;
+                for (String alist1 : alist) {
+                    if (i == comboID) {
+
+                        txtFirmaPuan.setText(rs.getString("puan"));
+                        ipuanGetir(null);
+                        break;
+                    }
+                    i++;
+
+                }
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ÖdemeBelirlemeController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+      
+    }
+
+    @FXML
+    public void ipuanGetir(ActionEvent evt) {
+        
+         int comboID = cmbAd.getSelectionModel().getSelectedIndex();
+
+          for (Firmalar alist1 : firmaListe) {
+             // System.out.println("sdfsdfsd sdfsdf s" + alist1.toString());
+        }
+          Firmalar gelen = firmaListe.get(comboID);
+          int id = gelen.getId();
+             // System.out.println("asdasdasdasd"+id);
+          
+          
+          
+         try {
+                 ResultSet rs=baglan().executeQuery("select puan_degeri from puanlama where id =  '"+id+"' ");
+                 while(rs.next()){
+                 
+                     txtİlanPuan.setText(rs.getString("puan_degeri"));
+               
+                 }
+             } catch (SQLException ex) {
+                 Logger.getLogger(ÖdemeBelirlemeController.class.getName()).log(Level.SEVERE, null, ex);
+             }
+       
+    }
+
+    public void getir() {
+
+        try {
+            ResultSet rs = baglan().executeQuery("select unvan,puan from firmalar");
+            while (rs.next()) {
+                cmbAd.getItems().add(rs.getString("unvan"));
+                alist.add(rs.getString("puan"));
+
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ÖdemeBelirlemeController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+    
+    @FXML
+    public void hesapla(ActionEvent evt) {
+    
+        String a = txtFirmaPuan.getText();
+        String b = txtİlanPuan.getText();
+        
+        lblBedel.setText((Double.valueOf(a)*Double.valueOf(b))+"");
+        
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        firmabilgileriGetir();
+        getir();
+    }
+
+}
--- src/javafx_ik/Kullanicilar/Firmalar.java
+++ src/javafx_ik/Kullanicilar/Firmalar.java
@@ -1,7 +1,8 @@
 package javafx_ik.Kullanicilar;
 
 public class Firmalar extends Kisiler{
-    
+   
+    private int id;
     private int bolum_id;
     private String unvan;
     private String yetkili_adi;
@@ -15,8 +16,76 @@ public class Firmalar extends Kisiler{
     private String mail;
     private String date;
 
+  public Firmalar(int id, int bolum_id, String unvan, String yetkili_adi, String yetkili_soyadi, String telefon, String gsm, String logo, int puan, String aciklama, String web_adresi, String mail, String date) {
+        this.id = id;
+        this.bolum_id = bolum_id;
+        this.unvan = unvan;
+        this.yetkili_adi = yetkili_adi;
+        this.yetkili_soyadi = yetkili_soyadi;
+        this.telefon = telefon;
+        this.gsm = gsm;
+        this.logo = logo;
+        this.puan = puan;
+        this.aciklama = aciklama;
+        this.web_adresi = web_adresi;
+        this.mail = mail;
+        this.date = date;
+    }
+  
   
 
 // Sadece Firmalar'ı ilgilendiren metodlar...
+
+    public int getId() {
+        return id;
+    }
+
+    public int getBolum_id() {
+        return bolum_id;
+    }
+
+    public String getUnvan() {
+        return unvan;
+    }
+
+    public String getYetkili_adi() {
+        return yetkili_adi;
+    }
+
+    public String getYetkili_soyadi() {
+        return yetkili_soyadi;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public String getGsm() {
+        return gsm;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public int getPuan() {
+        return puan;
+    }
+
+    public String getAciklama() {
+        return aciklama;
+    }
+
+    public String getWeb_adresi() {
+        return web_adresi;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getDate() {
+        return date;
+    }
    
 }
